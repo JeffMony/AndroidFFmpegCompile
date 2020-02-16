78ec03f2d05aa02ce3d8e508d85868c5e1724c5a
Jeff Mony <jeffmony@163.com>
2020-02-16 21:19:28 +0800

    提交x264库文件

--- x264/build_android_x264.sh
+++ x264/build_android_x264.sh
@@ -1,48 +1,41 @@
-# !/bin/bash
+#!/bin/bash
+export NDK_ROOT=/Users/tianpeng/tools/android-ndk-r14b # 修改自己本地的ndk路径
 
-export NDK_ROOT=/home/jeffmony/tools/android-ndk-r14b # 修改自己本地的ndk路径
+build() {
+API=24
+ARCH=$1
+PLATFORM=$2
+SYSROOT=$NDK_ROOT/platforms/android-$API/arch-$ARCH/
+CROSS_PREFIX=$NDK_ROOT/toolchains/$PLATFORM-4.9/prebuilt/darwin-x86_64/bin/$PLATFORM-
+PREFIX=$(pwd)/android/$ARCH #自己指定一个输出目录
+rm -rf $(pwd)/android/$ARCH
 
-configure()
-{
-    CPU=$1
-    API=24
-    PREFIX=$(pwd)/android/$CPU
-    HOST=""
-    CROSS_PREFIX=""
-    SYSROOT=""
-    if [ "$CPU" == "armv7-a" ]
-    then
-        HOST=arm-linux
-        SYSROOT=$NDK_ROOT/platforms/android-$API/arch-arm/
-        CROSS_PREFIX=$NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/arm-linux-androideabi-
-    else
-        HOST=aarch64-linux
-        SYSROOT=$NDK_ROOT/platforms/android-$API/arch-arm64/
-        CROSS_PREFIX=$NDK_ROOT/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-
-    fi
-    ./configure \
-    --prefix=$PREFIX \
-    --host=$HOST \
-    --enable-pic \
-    --disable-asm \
-    --enable-static \
-    --enable-neon \
-    --extra-cflags="-fPIE -pie" \
-    --extra-ldflags="-fPIE -pie" \
-    --cross-prefix=$CROSS_PREFIX \
-    --sysroot=$SYSROOT
+if [ "$ARCH" == "arm" ];
+then
+    HOST=arm-linux
+elif [ "$ARCH" == "arm64" ];
+then
+    HOST=aarch64-linux
+fi
+
+./configure \
+--prefix=$PREFIX \
+--host=$HOST \
+--enable-pic \
+--disable-asm \
+--enable-static \
+--cross-prefix=$CROSS_PREFIX \
+--sysroot=$SYSROOT
 }
 
-build()
-{
-    make clean
-    cpu=$1
-    echo "build $cpu"
+# build armv7a
+build arm arm-linux-androideabi
+make clean
+make -j4
+make install
 
-    configure $cpu
-    make -j4
-    make install
-}
-
-build arm64
-build armv7-a
+# build armv8a
+build arm64 aarch64-linux-android
+make clean
+make -j4
+make install
